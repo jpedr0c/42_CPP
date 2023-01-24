979adf447ce498d9edf4156f0147ca341d409b8e
jpedr0c <jpccarvalho2210@gmail.com>
2023-01-24 17:41:47 -0300

    add color library

--- module_00/ex01/Colors.hpp
+++ module_00/ex01/Colors.hpp
@@ -11,84 +11,72 @@
 /* ************************************************************************** */
 
 #ifndef COLORS_HPP
-#define COLORS_HPP
-#include <iostream>
+# define COLORS_HPP
 
-namespace color {
-                    
-  const std::string gray  = "\e[30;10m", // normal
-                    grayn = "\e[30;1m", // negrito
-                    grayf = "\e[30;2m", // fraco
-                    grayi = "\e[30;3m", // itálico
-                    grays = "\e[30;4m", // sublinhado
-                    grayp = "\e[30;5m", // piscando
-                    grayb = "\e[30;7m", // background
-                    grayc = "\e[30;9m", // cancelado
-
-                    red  = "\e[31;10m", // normal
-                    redn = "\e[31;1m", // negrito
-                    redf = "\e[31;2m", // fraco
-                    redi = "\e[31;3m", // itálico
-                    reds = "\e[31;4m", // sublinhado
-                    redp = "\e[31;5m", // piscando
-                    redb = "\e[31;7m", // background
-                    redc = "\e[31;9m", // cancelado
-
-                    green  = "\e[32;10m", // normal
-                    greenn = "\e[32;1m", // negrito
-                    greenf = "\e[32;2m", // fraco
-                    greeni = "\e[32;3m", // itálico
-                    greens = "\e[32;4m", // sublinhado
-                    greenp = "\e[32;5m", // piscando
-                    greenb = "\e[32;7m", // background
-                    greenc = "\e[32;9m", // cancelado
-
-                    yellow  = "\e[33;10m", // normal
-                    yellown = "\e[33;1m", // negrito
-                    yellowf = "\e[33;2m", // fraco
-                    yellowi = "\e[33;3m", // itálico
-                    yellows = "\e[33;4m", // sublinhado
-                    yellowp = "\e[33;5m", // piscando
-                    yellowb = "\e[33;7m", // background
-                    yellowc = "\e[33;9m", // cancelado
-
-                    blue  = "\e[34;10m", // normal
-                    bluen = "\e[34;1m", // negrito
-                    bluef = "\e[34;2m", // fraco
-                    bluei = "\e[34;3m", // itálico
-                    blues = "\e[34;4m", // sublinhado
-                    bluep = "\e[34;5m", // piscando
-                    blueb = "\e[34;7m", // background
-                    bluec = "\e[34;9m", // cancelado
-
-                    purple  = "\e[35;10m", // normal
-                    purplen = "\e[35;1m", // negrito
-                    purplef = "\e[35;2m", // fraco
-                    purplei = "\e[35;3m", // itálico
-                    purples = "\e[35;4m", // sublinhado
-                    purplep = "\e[35;5m", // piscando
-                    purpleb = "\e[35;7m", // background
-                    purplec = "\e[35;9m", // cancelado
-
-                    cyan  = "\e[36;10m", // normal
-                    cyann = "\e[36;1m", // negrito
-                    cyanf = "\e[36;2m", // fraco
-                    cyani = "\e[36;3m", // itálico
-                    cyans = "\e[36;4m", // sublinhado
-                    cyanp = "\e[36;5m", // piscando
-                    cyanb = "\e[36;7m", // background
-                    cyanc = "\e[36;9m", // cancelado
-
-                    white  = "\e[38;10m", // normal
-                    whiten = "\e[38;1m", // negrito
-                    whitef = "\e[38;2m", // fraco
-                    whitei = "\e[38;3m", // itálico
-                    whites = "\e[38;4m", // sublinhado
-                    whitep = "\e[38;5m", // piscando
-                    whiteb = "\e[38;7m", // background
-                    whitec = "\e[38;9m", // cancelado
-
-                    off = "\e[m"; // desliga
-}
+#define		GRAY	"\e[30;10m" // normal
+#define		GRAYN	"\e[30;1m" // negrito
+#define		GRAYF 	"\e[30;2m" // fraco
+#define		GRAYI	"\e[30;3m" // itálico
+#define		GRAYS	"\e[30;4m" // sublinhado
+#define		GRAYP	"\e[30;5m" // piscando
+#define		GRAYB	"\e[30;7m" // background
+#define		GRAYC	"\e[30;9m" // cancelado
+#define		RED		"\e[31;10m" // normal
+#define		REDN	"\e[31;1m" // negrito
+#define		REDF	"\e[31;2m" // fraco
+#define		REDI	"\e[31;3m" // itálico
+#define		REDS	"\e[31;4m" // sublinhado
+#define		REDP	"\e[31;5m" // piscando
+#define		REDB	"\e[31;7m" // background
+#define		REDC	"\e[31;9m" // cancelado
+#define		GREEN	"\e[32;10m" // normal
+#define		GREENN	"\e[32;1m" // negrito
+#define		GREENF	"\e[32;2m" // fraco
+#define		GREENI	"\e[32;3m" // itálico
+#define		GREENS	"\e[32;4m" // sublinhado
+#define		GREENP	"\e[32;5m" // piscando
+#define		GREENB	"\e[32;7m" // background
+#define		GREENC	"\e[32;9m" // cancelado
+#define		YELLOW	"\e[33;10m" // normal
+#define		YELLOWN	"\e[33;1m" // negrito
+#define		YELLOWF	"\e[33;2m" // fraco
+#define		YELLOWI	"\e[33;3m" // itálico
+#define		YELLOWS	"\e[33;4m" // sublinhado
+#define		YELLOWP	"\e[33;5m" // piscando
+#define		YELLOWB	"\e[33;7m" // background
+#define		YELLOWC	"\e[33;9m" // cancelado
+#define		BLUE	"\e[34;10m" // normal
+#define		BLUEN	"\e[34;1m" // negrito
+#define		BLUEF	"\e[34;2m" // fraco
+#define		BLUEI	"\e[34;3m" // itálico
+#define		BLUES	"\e[34;4m" // sublinhado
+#define		BLUEP	"\e[34;5m" // piscando
+#define		BLUEB	"\e[34;7m" // background
+#define		BLUEC	"\e[34;9m" // cancelado
+#define		PURPLE	"\e[35;10m" // normal
+#define		PURPLEN	"\e[35;1m" // negrito
+#define		PURPLEF	"\e[35;2m" // fraco
+#define		PURPLEI	"\e[35;3m" // itálico
+#define		PURPLES	"\e[35;4m" // sublinhado
+#define		PURPLEP	"\e[35;5m" // piscando
+#define		PURPLEB	"\e[35;7m" // background
+#define		PURPLEC	"\e[35;9m" // cancelado
+#define		CYAN	"\e[36;10m" // normal
+#define		CYANN	"\e[36;1m" // negrito
+#define		CYANF	"\e[36;2m" // fraco
+#define		CYANI	"\e[36;3m" // itálico
+#define		CYANS	"\e[36;4m" // sublinhado
+#define		CYANP	"\e[36;5m" // piscando
+#define		CYANB	"\e[36;7m" // background
+#define		CYANC	"\e[36;9m" // cancelado
+#define		WHITE	"\e[38;10m" // normal
+#define		WHITEN	"\e[38;1m" // negrito
+#define		WHITEF	"\e[38;2m" // fraco
+#define		WHITEI	"\e[38;3m" // itálico
+#define		WHITES	"\e[38;4m" // sublinhado
+#define		WHITEP	"\e[38;5m" // piscando
+#define		WHITEB	"\e[38;7m" // background
+#define		WHITEC	"\e[38;9m" // cancelado
+#define		DEFAULT "\e[m" // padrão
 
 #endif
