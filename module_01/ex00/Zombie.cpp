9e709766b5cf9b9d2f92e25c7d5b86d4ddda801a
jpedr0c <jpccarvalho2210@gmail.com>
2023-01-26 11:40:43 -0300

    ex00 finish

---
+++ module_01/ex00/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocardos <jocardos@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 10:05:32 by jocardos          #+#    #+#             */
+/*   Updated: 2023/01/26 10:05:32 by jocardos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) 
+{
+    this->nameZombie = name;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << this->nameZombie << ": was destroyed!\n";
+}
+
+void Zombie::announce()
+{
+    std::cout << this->nameZombie << ": BraiiiiiiinnnzzzZ...\n";
+}
