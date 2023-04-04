9b8eea78be65b3c030a724649d52732176acb6ea
JPedroc17 <jpccarvalho2210@gmail.com>
2023-04-04 10:47:59 -0300

    ex00 finish

--- module_03/ex00/ClapTrap.hpp
+++ module_03/ex00/ClapTrap.hpp
@@ -18,11 +18,6 @@
 
 class ClapTrap
 {
-    private:
-        std::string name;
-        unsigned int hitPoints;
-        unsigned int energyPoints;
-        unsigned int attackDamage;
     public:
         ClapTrap();
         ClapTrap(std::string name);
@@ -33,6 +28,11 @@ class ClapTrap
         void    attack(const std::string &target);
         void    takeDamage(unsigned int amount);
         void    beRepaired(unsigned int amount);
+    private:
+        std::string name;
+        unsigned int hitPoints;
+        unsigned int energyPoints;
+        unsigned int attackDamage;
 };
 
 #endif
